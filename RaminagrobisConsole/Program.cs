9e9794ce8822f113cdb0944673c1100af0dfc1a5
ValentinBonneau <valentin.bonneau@epsi.fr>
2021-10-28 14:27:56 +0200

    Ajoutez des fichiers projet.

---
+++ RaminagrobisConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RaminagrobisConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
