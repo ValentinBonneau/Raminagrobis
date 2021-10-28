74ac2574920a9099dd45ad67f0646e89f2f8af87
ValentinBonneau <valentin.bonneau@epsi.fr>
2021-10-28 15:36:24 +0200

    creation fichier DAL adhérent

---
+++ Raminagrobis.DAL/Adherent_DAL.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raminagrobis.DAL
+{
+    public class Adherent_DAL
+    {
+        public int ID { get; set; }
+        public String nom { get; set; }
+        public String prenomC { get; set; }
+        public String nomC { get; set; }
+        public String sexeC { get; set; }
+        public String email { get; set; }
+        public String addresse { get; set; }
+        public DateTime dateA { get; set; }
+
+    }
+}
