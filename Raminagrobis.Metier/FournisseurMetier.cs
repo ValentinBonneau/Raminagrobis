2e2c73933a49c587859d1a10b5c66fec213510e3
ValentinBonneau <valentin.bonneau@epsi.fr>
2021-11-03 16:04:44 +0100

    Classe metier fournisseur

--- Raminagrobis.Metier/FournisseurMetier.cs
+++ Raminagrobis.Metier/FournisseurMetier.cs
@@ -1,8 +1,90 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Raminagrobis.Metier
 {
     public class FournisseurMetier
     {
+        public string Nom { get; private set; }
+        public string NomC { get; private set; }
+        public string PrenomC { get; private set; }
+        public string Email { get; private set; }
+        public string Adresse { get; private set; }
+
+        public List<ReferenceMetier> Refs { get; private set; }
+        public FournisseurMetier(string nom, string nomC, string prenomC, string email, string adresse)
+        {
+            Nom = nom;
+            NomC = nomC;
+            PrenomC = prenomC;
+            Email = email;
+            Adresse = adresse;
+            Refs = new List<ReferenceMetier>();
+        }
+
+        public void Ajouter_References(List<ReferenceMetier> refs) {
+            foreach (var item in refs)
+            {
+                Refs.Add(item);
+            }
+        }
+
+        public void Ajouter_References_From_CSV(StreamReader sr)
+        {
+            var line = sr.ReadLine();
+            var columnName = line.Split(";");
+
+            if (columnName.Length == 3)
+            {
+                while (!sr.EndOfStream)
+                {
+                    var column = sr.ReadLine().Split(";");
+                    string refs = "", nom = "", marque = "";
+
+                    for (int i = 0; i < 3; i++)
+                    {
+                        switch (columnName[i])
+                        {
+                            case "reference":
+                                refs = column[i];
+                                break;
+                            case "libelle":
+                                nom = column[i];
+                                break;
+                            case "marque":
+                                marque = column[i];
+                                break;
+                            default:
+                                throw new Exception($"Syntaxe incorecte dans le nom de la colone : {columnName[i]} ");
+                        }
+                    }
+                    if (refs == "" || nom == "" || marque == "")
+                    {
+                        throw new Exception($"l'une des ligne du Csv ne contien pas d'information");
+                    }
+                    else
+                    {
+                        this.Ajouter_Reference(new ReferenceMetier(refs, nom, marque));
+
+                    }
+                }
+            }
+            else
+            {
+                throw new Exception("Le Csv fournit pour les references d'un fournisseur n'est pas au bon format ");
+            }
+            
+        }
+
+        public void Ajouter_Reference(ReferenceMetier refs)
+        {
+                Refs.Add(refs);
+        }
+
+
+
+
+
     }
 }
---
+++ Raminagrobis.Metier/ReferenceMetier.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raminagrobis.Metier
+{
+    
+    public class ReferenceMetier
+    {
+        public string Refs { get; private set; }
+
+        public string Nom { get; private set; }
+
+        public string Marque { get; private set; }
+
+        public ReferenceMetier(string refs, string nom, string marque)
+        {
+            Refs = refs;
+            Nom= nom;
+            Marque = marque;
+        }
+    }
+}
