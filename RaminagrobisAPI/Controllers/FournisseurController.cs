f91df2257405c63b9f7dac5d8876e4dcd6f41d4d
ValentinBonneau <valentin.bonneau@epsi.fr>
2021-11-15 09:39:28 +0100

    Start Création API

---
+++ RaminagrobisAPI/Controllers/FournisseurController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RaminagrobisAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FournisseurController : ControllerBase
+    {
+        // GET: api/<FournisseurController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<FournisseurController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<FournisseurController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<FournisseurController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<FournisseurController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
